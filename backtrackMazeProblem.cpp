54ce5cfeb4c7a87eba5b44fdd845bfc6851a858f
arun-jyothish <arunjyothish.mec@gmail.com>
2022-12-15 18:49:52 +0530

    random matrix generation

--- backtrackMazeProblem.cpp
+++ backtrackMazeProblem.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <fstream>
 #include <vector>
 
 using namespace std; 
 
+void errLog(int);
 template<typename T>
 class Solution{
 	private:
 		/* vector<vector<int>> mat; */
 		T mat;
 	public:
-		void print();
+		void print(ostream& out = cout);
 		Solution(T mat){
 			if( typeid(T) != typeid(vector<vector<int>>) ){
 				cout << 
 					"Wrong Type Matrix Recieved"<<endl;
+					errLog(112);
 				return;
 			}
 			this->mat = mat;
@@ -23,8 +28,52 @@ class Solution{
 
 };
 
+void fstore(auto obj){
+	// fstore function receives any object and perform _obj.print(store)_  operation
+	// in which store is a output file stream.
+	//
+	// print() function should be print(ostream& out) where _out_ recieves file stream & when calling this function
+	//
+	
+	/* fstream store("out.txt",ios::out); */
+	fstream store("out.txt",ios::app);
+	obj.print(store);
+	store.close();
+}
+int otherMain();
+
+// Generate Random bool matrix , when row and coloumn are specified.
+vector <vector<int>> generateMatrix(int rw,int cl){
+	vector<int> arr;
+	vector <vector<int>> matrix;
+	for (int i=0; i< rw; ++i){
+		for (int j=0; j< cl; ++j){
+			bool state = rand()%2 == 0;
+			arr.push_back(state);
+		}
+		matrix.push_back(arr);
+		arr.clear();
+	}
+	cout << " in gen cl: " << matrix.size()<< " rw: "<< matrix[0].size()<<endl;
+	return matrix;
+}
+
+// Generate random matrix
+vector <vector<int>> generateMatrix(){
+	 int cl = 3+rand()%5;	// random number bw 3 and 8
+	 int rw = 3+rand()%5;
+	 cout << "cl: " <<cl<< " rw: "<<rw <<endl;
+	 return generateMatrix(rw,cl);
+}
 
 int main(int argc, char *argv[])
+{
+	srand(time(0));
+	otherMain();
+	/* generateMatrix(); */
+	return 0;
+}
+int otherMain()
 {
 	vector<vector<int>> mat{
 			{1, 1, 0, 0},
@@ -32,27 +81,35 @@ int main(int argc, char *argv[])
 			{0, 0, 1, 1},
 			{0, 1, 0, 1},
 	};
-
-	Solution S1(mat);
-	/* S1.print(); */
+	auto ranMat = generateMatrix();
+	int rw = ranMat.size();
+	int cl = ranMat[0].size();
+	cout << "cl: " <<cl<< " rw: "<<rw <<endl;
+	
+	errLog(0xFF01);
+	errLog(0x0A01);
+	errLog(0x0FF1);
+	Solution S1(ranMat);
+	S1.print();
+	fstore(S1);
 	/* auto ans = S1.allPaths(); */
 	return 0;
 }
 
 
 template<typename T>
-void Solution<T>::print(){
+void Solution<T>::print(ostream& out){
 	if ( typeid(this->mat) != typeid(vector<vector<int>>))
 	{
-	cout << "type doesn't match" <<endl;
+	out << "type doesn't match" <<endl;
 	return;
 	}
-	cout << "\tGIVEN MATRIX "<< endl<<endl;
+	out << "\tGIVEN MATRIX "<< endl<<endl;
 	for (auto arr: mat){
 		for ( auto val: arr ){
-			cout <<"\t"<< val;
+			out <<"\t"<< val;
 		}
-		cout << endl;
+		out << endl;
 	}
 }
 
@@ -65,4 +122,34 @@ vector<vector<pair<int,int>>> Solution<T>::allPaths(){
 	li.push_back(pr);
 	ans.push_back(li);
 	return ans;
+}
+
+void errLog(int errInt){
+	fstream err("errLog.txt",ios::app);
+	err << errInt ;
+	/* err.width(10); */
+	err << "\t:";
+	/* err.width(20); */
+	if (errInt > 0xAA00){
+		err <<"\t\t\t";
+	}
+	else if (errInt > 0x0A00){
+		err <<"\t\t";
+	}
+	else if (errInt > 0x00FF){
+		err <<"\t";
+	}
+	switch(errInt)
+	{
+		case 0x00FA:
+			err << "Matrix type doesn't match" ;
+			break;
+		case 0xFFFF:
+			err << "recursion timeout" ;
+			break;
+		default:
+			/* err.width(6); */
+			err << "error";
+	}
+	err << endl;
 }
