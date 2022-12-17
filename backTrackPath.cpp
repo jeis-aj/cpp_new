5386c76fd52dc0f02b084b8ffec39a3262aa966a
arun-jyothish <arunjyothish.mec@gmail.com>
2022-12-17 12:22:31 +0530

    backtrack,visualPath
    partial working backtrack, visual path fn create

--- backTrackPath.cpp
+++ backTrackPath.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <map>
 #include <vector>
 using namespace std; 
 
-/* vector<vector<pair<int,int>>> getPath(auto mat, auto Spr, auto Dpr,auto Ppr); */
 void getPath(auto mat,auto & result, auto Spr, auto Dpr,auto Ppr);
 void printPaths(auto mat,ostream& out = cout);
 ostream& fn(ostream& = cout);
@@ -12,7 +12,7 @@ bool operator==(const pair<int,int>& one,const pair<int,int>& two){
 		return false;
 	return true;
 }
-vector<pair<int,int>> mkPairlist(auto mat, pair<int,int>& Npr ,pair<int,int>& Ppr){
+vector<pair<int,int>> mkPairlist(auto mat, pair<int,int> Npr ,pair<int,int> Ppr){
 	pair<int,int> pr;
 	vector<pair<int,int>> rtPr;
 	int rwSize = mat.size();
@@ -23,7 +23,7 @@ vector<pair<int,int>> mkPairlist(auto mat, pair<int,int>& Npr ,pair<int,int>& Pp
 	pair<int,int> bottomPr = {Npr.first, Npr.second+1};
 	pair<int,int> topPr = {Npr.first, Npr.second-1};
 
-	vector<pair<int,int>> direction = { rightPr,leftPr,topPr,bottomPr };
+	vector<pair<int,int>> direction = { rightPr,bottomPr,leftPr,topPr };
 
 	for (auto pr: direction){
 		if ( pr.first < rwSize && pr.first >= 0 ){
@@ -36,44 +36,81 @@ vector<pair<int,int>> mkPairlist(auto mat, pair<int,int>& Npr ,pair<int,int>& Pp
 	return rtPr;
 }
 
+void printList(auto vect){
+	for ( auto Spr: vect ){
+		cout << "x:" <<Spr.first<<" y:"<<Spr.second <<" -> ";
+	}
+	cout <<endl;}
+void printVisual(auto mat,auto vect);
 int main(int argc, char *argv[])
 {
 	srand(time(0));
 	vector<vector<int>> mat{
-			{1, 1, 0, 0},
+		{1, 1, 0, 0},
 			{0, 1, 1, 0},
 			{0, 0, 1, 1},
 			{0, 1, 0, 1},
 	};
 	pair<int,int> Npr{0,0};
 	pair<int,int> Spr{0,0};
-	pair<int,int> Dpr{2,2};
+	pair<int,int> Dpr{3,3};
 	pair<int,int> Ipr{-1,-1};
 
 	vector<vector<pair<int,int>>> result;
 	getPath(mat,result,Spr,Dpr,Ipr);
+	/* auto vect = mkPairlist(mat,Spr,Npr); */
 	/* cout << "hello" <<endl; */
+	printVisual(mat,result);
 	printPaths(result);
 	return 0;
 }
 
 
 void getPath(auto mat,auto & result, auto Spr, auto Dpr,auto Ppr) {
-	vector<pair<int,int>> temp;
+	static vector<pair<int,int>> temp;
+	static map <pair<int,int>,bool> mp;
 	if (Spr == Dpr){
-		result.push_back(temp);
-	}
-	else if(temp.front() == temp.back());
-	else{
+		/* cout << "dest reach" <<endl; */
 		temp.push_back(Spr);
+		result.push_back(temp);
+		temp.pop_back();
+	}
+	else if(mp[Spr]){
+ 	/* cout << "same node"  <<endl; */
+	}
+	else{
+		/* cout << "else"  <<endl; */
+		temp.push_back(Spr);
+		mp[Spr] = true;
+		/* printList(temp); */
 		auto nextPathlist = mkPairlist(mat,Spr,Ppr);
+		/* printList(nextPathlist); */
 		for (auto CurrentPair: nextPathlist){
 			getPath(mat,result,CurrentPair,Dpr,Spr);
 		}
+		temp.pop_back();
 	}
-	temp.pop_back();
 }
-
+void printVisual(auto mat,auto vect){
+	map<pair<int,int>,bool> mp; 
+	for (auto cont: vect){
+		cout <<endl<<"New Path:"<<endl;
+		for (auto Mpr: cont){
+			mp[Mpr] = true;
+		}
+			for(int i=0; i <mat.size(); ++i){
+				for(int j=0; j <mat[0].size(); ++j){
+						if(mp[{i,j}]){
+							cout <<" * ";
+						}
+						else
+							cout << "   ";
+					}
+				cout<<endl;
+		}
+			mp.clear();
+	}
+}
 void printPaths(auto mat,ostream& out){
 	for (auto arr: mat){
 		for(auto pr:arr ){
