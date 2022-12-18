20e361046def4a45e5d9c8d33b0ceca5b6966f5d
arun-jyothish <arunjyothish.mec@gmail.com>
2022-12-18 20:02:13 +0530

    Commented, just before threading

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <algorithm>
 #include <map>
 #include <time.h>
@@ -7,34 +8,38 @@
 #include <vector>
 
 using namespace std; 
+typedef pair<int,int> ind;				// defined an alternative name to pair<int,int> -> ind, so that tedious typing can be reduced
+char ratSymbol = '&';								// rat symbol in matrix
+char cheeseSymbol = '@';							// cheese symbol in matrix
+char pathValidSymbol = '.';							// valid path symbol in matrix
+char pathBlockSymbol = ' ';							// path block symbol in matrix
 
 void errLog(int);
-
-		vector <vector<char>> generateMatrix();
-		vector <vector<char>> generateMatrix(int rw,int cl);
+vector <vector<char>> generateMatrix();
+vector <vector<char>> generateMatrix(int rw,int cl);
 
 template<typename T>
-class Solution{
+class Solution{												// main pathfinder class begins here
 	private:
 		/* vector<vector<int>> mat; */
 		T mat;
-		pair<int,int> SourcePair;
-		pair<int,int> DestPair;
-		pair<int,int> Initialpair{-1,-1};
-		vector<vector<pair<int,int>>> result;
-		pair<int,int> Spr;
-		pair<int,int> Ppr;
+		ind SourcePair;
+		ind DestPair;
+		ind Initialpair{-1,-1};
+		vector<vector<ind>> result;
+		ind Spr;
+		ind Ppr;
 	public:
 		void fstore(string fileName,void (*fn)(ostream&));
-		vector<pair<int,int>> mkPairlist( auto Npr , auto Ppr);
+		vector<ind> mkPairlist( auto Npr , auto Ppr);
 		void getPath();
+		void getPath(auto temp, auto mp);
 		void printVisual(int maxCount = -1);
 		void printPaths(ostream& out=cout);
 		void print(ostream& out = cout);
 		void sortSolution();
-		pair<pair<int,int>,pair<int,int>> randomPositionTarget();
-		/* void putTargets (pair<pair<int,int>,pair<int,int>> TargetPairs); */
-		vector<vector<pair<int,int>>> allPaths();
+		pair<ind,ind> randomPositionTarget();
+		vector<vector<ind>> allPaths();
 		T getMatrix()	{
 			return mat;
 		}
@@ -47,29 +52,22 @@ class Solution{
 			this->mat = mat;
 		}
 };
-/*
-template <typename T>
-void Solution<T>::putTargets (pair<pair<int,int>,pair<int,int>> TargetPairs){
-	SourcePair = TargetPairs.first;
-	DestPair = TargetPairs.second;
-}
-*/
-template <typename T>
-void Solution<T>::sortSolution(){
+template <typename T>					
+void Solution<T>::sortSolution(){								// Sorting solution vector ( result ), for finding shortest path first
 	sort(result.begin(),result.end(),[](auto one,auto two){
 			return one.size() < two.size();
 			});
 }
+
 // put random rat and cheese
 template <typename T>
-pair<pair<int,int>,pair<int,int>> Solution<T>::randomPositionTarget(){
+pair<ind,ind> Solution<T>::randomPositionTarget(){				// function manipulate matrix (mat), put random rat and cheese somewhere in the valid matrix location
 	SourcePair.first = rand()%mat.size();
 	SourcePair.second = rand()%mat[0].size();
-	mat[SourcePair.first][SourcePair.second]	= '&';		// rat
-							//
+	mat[SourcePair.first][SourcePair.second]	= ratSymbol;		// rat
 	DestPair.first = rand()%mat.size();
 	DestPair.second = rand()%mat[0].size();
-	mat[DestPair.first][DestPair.second]	= '@';		// cheese
+	mat[DestPair.first][DestPair.second]	= cheeseSymbol;		// cheese
 	Spr = SourcePair;
 	return {SourcePair,DestPair};
 }
@@ -88,14 +86,16 @@ void Solution<T>::fstore(string fileName,void (*fn)(ostream&)){
 }
 
 // Generate Random bool matrix , when row and coloumn are specified.
-//
+
 vector<vector<char>> generateMatrix(int rw,int cl){
+	// generate matrix based on rw and cl size ( row size && coloumn size)
+	// and put random paths and blocks
 	vector<char> arr;
 	vector <vector<char>> matrix;
 	for (int i=0; i< rw; ++i){
 		for (int j=0; j< cl; ++j){
-			bool state = rand()%5 <= 1;			// probability of tracks 1/7
-			char ch = state?'.':' ';
+			bool state = rand()% 10 < 6;										// probability of tracks 1/7
+			char ch = state? pathValidSymbol : pathBlockSymbol ;
 			arr.push_back(ch);
 		}
 		matrix.push_back(arr);
@@ -104,27 +104,28 @@ vector<vector<char>> generateMatrix(int rw,int cl){
 	return matrix;
 }
 
-// Generate random matrix
+// Generate random matrix without specifying row and coloumn size ( randomly generate )
 vector <vector<char>> generateMatrix(){
-	 int cl = 8+rand()%9;	// random number bw 3 and 8
-	 int rw = 8+rand()%9;
+	 int cl = 15 +rand()%6;	// random number bw 3 and 8
+	 int rw = 9 +rand()%6;
 	 /* int cl = 3+rand()%3;	// random number bw 3 and 8 */
 	 /* int rw = 3+rand()%3; */
 	 return generateMatrix(rw,cl);
 }
 
-int otherMain();
+int randomPathFinder();
 int main(int argc, char *argv[])
 {
 	char ch;
-	while(ch != 'x'){
-		otherMain();
+	while(ch != 'x'){			// Execute multiple times until key character x recieved
+		randomPathFinder();
 		cout << endl <<"Press 'X' to quit " <<endl;
 		cin.get(ch);
 	}
 	return 0;
 }
-int otherMain()
+
+int randomPathFinder()			// main function to Execute random path find
 {
 	srand(time(0));
 	auto ranMat = generateMatrix();
@@ -132,24 +133,26 @@ int otherMain()
 	auto pos = S1.randomPositionTarget();
 	S1.print();
 	S1.getPath();
-	/* S1.printPaths(); */
-	S1.sortSolution();
-	S1.printVisual(3);
+
+	/* S1.printPaths(); */		// printing all paths by pairs
+
+	S1.sortSolution();			// sort solution matrix (result) so that shortest path will get in first
+	S1.printVisual(3);			// visually prints paths, if any integer value provided that many paths will be printed, second argument by default is cout ( ostream& )
 	cout << "END" <<endl;
-	/* cout << S1.result.empty(); */
+
 	/*
 	errLog(0xFF01);
 	errLog(0x0A01);
-	errLog(0x0FF1);
-	S1.print();
+	errLog(0x0FF1);				// error logs can be included for debugging purpose
 	*/
+
 	return 0;
 }
 
 
 template<typename T>
 void Solution<T>::print(ostream& out){	//		prints matrix in in its original form
-	if ( typeid(this->mat) != typeid(vector<vector<char>>))
+	if ( typeid(this->mat) != typeid(vector<vector<char>>))			
 	{
 	out << "type doesn't match" <<endl;
 	return;
@@ -158,7 +161,7 @@ void Solution<T>::print(ostream& out){	//		prints matrix in in its original form
 	out <<"ROWS: " << mat.size()<<" COLOUMS: "<< mat[0].size()<<endl;
 	for (auto arr: mat){
 		for ( auto val: arr ){
-			out <<"  "<< val;
+			out <<" "<< val;
 		}
 		out << endl;
 	}
@@ -166,42 +169,52 @@ void Solution<T>::print(ostream& out){	//		prints matrix in in its original form
 }
 
 template<typename T>
-void Solution<T>::getPath() {
-	static vector<pair<int,int>> temp;
-	static map <pair<int,int>,bool> mp;
+void Solution<T>::getPath() {			// intial fn before executing recursive call of getPath
+	vector<ind> temp;
+	map <ind,bool> mp;
+	getPath(temp,mp);
+}
+
+template<typename T>
+vector<T> proc(auto obj){
+	static vector<T> vect;
+	vect.push_back(obj);
+	return vect;
+}
+
+template<typename T>
+void Solution<T>::getPath( auto temp, auto mp) {		// Recursively search for valid paths( backtrack/bruteforce )
 	if (Spr == DestPair){
-		/* cout << "dest reach" <<endl; */
 		temp.push_back(Spr);
 		result.push_back(temp);
-		/* mp.clear(); */
 		temp.pop_back();
 	}
-	else if(mp[Spr]){
- 	/* cout << "same node"  <<endl; */
-	}
+	else if(mp[Spr]);
 	else{
-		/* cout << "else"  <<endl; */
 		temp.push_back(Spr);
 		mp[Spr] = true;
-		/* printList(temp); */
 		auto nextPathlist = mkPairlist(Spr,Ppr);
-		/* printList(nextPathlist); */
 		for (auto CurrentPair: nextPathlist){
 			Spr = CurrentPair;
-			getPath();
+			/* auto obj = thread(getPath,temp,mp); */						// multithreading code
+			 /* auto vect = proc(obj); */
+			getPath(temp,mp);												// single core/thread code
 		}
-		mp[temp.back()]=false;
-		temp.pop_back();
 	}
+	mp[Spr] = false;
+	temp.pop_back();
+	/* for(auto obj:vect){ */
+	/* 	obj.join(); } */
 }
-// print obtained paths
+
+// print obtained paths in text
 template<typename T>
 void Solution<T>::printPaths(ostream& out){		// prints Solution vector (result)
 	if(result.empty()){
 		cout<< "No Path Available " <<endl;
 		return;
 	}
-	for (auto arr: result){
+	for (auto arr: result){						// way of formating path ( text output )
 		for(auto pr:arr ){
 			out << "("<<pr.first << "," <<pr.second <<")";
 			out << "->" ;
@@ -209,7 +222,8 @@ void Solution<T>::printPaths(ostream& out){		// prints Solution vector (result)
 		out <<endl;
 	}
 }
-void errLog(int errInt){
+
+void errLog(int errInt){									// error logs for easy debugging 
 	fstream err("errLog.txt",ios::app);
 	err << errInt ;
 	/* err.width(10); */
@@ -220,7 +234,7 @@ void errLog(int errInt){
 	}
 	else if (errInt > 0x0A00){
 		err <<"\t\t";
-	}
+	}														// hierarchial visualization of errors 
 	else if (errInt > 0x00FF){
 		err <<"\t";
 	}
@@ -240,22 +254,22 @@ void errLog(int errInt){
 }
 
 template<typename T>
-void Solution<T>::printVisual(int maxCount){
+void Solution<T>::printVisual(int maxCount){				// visually print all valid path matrix ( result matrix in the form of  - vector<vector<pair<int,int>>> - )
 	int i = 0;
 	if(result.empty()){
 		cout<< "No Path Available " <<endl;
 		return;
 	}
 	cout << "Number of Solutions Found: "<< result.size() << endl;
-	pair<int,int> pr;
-	map<pair<int,int>,bool> mp; 
-	if ( maxCount != -1 ){
+	ind pr;
+	map<ind,bool> mp; 
+	if ( maxCount != -1 ){									// if no argument passed in function, default argument is -1, so it prints all paths. but if provided any, prints that many
 		cout << "List of top "<< maxCount << " Solutions"<< endl; }
 	for (auto cont: result){
 		if ( i == maxCount )
 			break;
 		++i;
-		cout <<endl<<"New Path: \t\t\t\t ###"<<endl;
+		cout <<endl<<"New Path: \t\t\t\t\t\t\t\t ###"<<endl;				// just to see if any solution achieved in glimps of look during execution
 		for (auto Mpr: cont){
 			mp[Mpr] = true;
 		}
@@ -263,16 +277,16 @@ void Solution<T>::printVisual(int maxCount){
 			for(int j=0; j <mat[0].size(); ++j){
 				pr = {i,j};
 				if ( pr == SourcePair){
-					cout <<" & ";
+					cout << " " << ratSymbol ;									// rat symbol
 				}
 				else if ( pr == DestPair){
-					cout <<" @ ";
+					cout << " " << cheeseSymbol ;								// cheese symbol
 				}
 				else if(mp[pr]){
-					cout <<" . ";
+					cout << " "<< pathValidSymbol ;							// valid path symbol can be changed here
 				}
 				else
-					cout << "   ";
+					cout << " "<< pathBlockSymbol ;							// path block symbol can be changed here
 			}
 			cout<<endl;
 		}
@@ -281,30 +295,26 @@ void Solution<T>::printVisual(int maxCount){
 }
 
 template<typename T>
-vector<pair<int,int>> Solution<T>::mkPairlist( auto Npr , auto Ppr){
-	pair<int,int> pr;
-	vector<pair<int,int>> rtPr;
+vector<ind> Solution<T>::mkPairlist( auto Npr , auto Ppr){				// mkpairlist creates vector of adjacent valid path
+	ind pr;																
+	vector<ind> rtPr;
 	int rwSize = mat.size();
 	int clSize = mat[0].size();
 
-	pair<int,int> rightPr = {Npr.first+1, Npr.second};
-	pair<int,int> leftPr= {Npr.first-1, Npr.second};
-	pair<int,int> bottomPr = {Npr.first, Npr.second+1};
-	pair<int,int> topPr = {Npr.first, Npr.second-1};
+	ind rightPr = {Npr.first+1, Npr.second};							// four direction of valid pair
+	ind leftPr= {Npr.first-1, Npr.second};
+	ind bottomPr = {Npr.first, Npr.second+1};
+	ind topPr = {Npr.first, Npr.second-1};
 
-	vector<pair<int,int>> direction = { rightPr,bottomPr,leftPr,topPr };
-
+	vector<ind> direction = { rightPr,bottomPr,leftPr,topPr };
 	for (auto pr: direction){
 		if ( pr.first < rwSize && pr.first >= 0 ){
-			if ( pr.second < clSize && pr.second >= 0 )
-				if ( pr == Ppr);
+			if ( pr.second < clSize && pr.second >= 0 )					// ignoring pair out of edge pair
+				if ( pr == Ppr);										// ignoring previously travelled pair
 				else
 				{
-					/* int  ch = mat[pr.first][pr.second]; */
 					char ch = mat[pr.first][pr.second];
-					/* if (ch == 1) */
-					/* if (ch == '.' || ch == '&' || ch == '@') */
-					if (ch != ' ')
+					if (ch != ' ')										// push back paths if character is not ' ', just not to get print path through block
 					rtPr.push_back(pr); 
 				}
 		}
