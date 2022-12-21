2279fcc45682850d517ecbae148adf8817efd52f
arun-jyothish <arunjyothish.mec@gmail.com>
2022-12-21 13:23:41 +0530

    patch

--- Arduino/sym/main.ino
+++ Arduino/sym/main.ino
@@ -1,5 +1,3 @@
-
-
 int fsr_pin = A0;
 int rainDrop_pin = A1;
 int tempS_pin = A2;
@@ -7,6 +5,7 @@ int Dehydrate_ind = 5;		// connected to an LED indication of dehydration
 
 void setup (){
 	pinMode( Dehydrate_ind ,OUTPUT);
+	pinMode( impactIndicator,OUTPUT);
 }
 
 struct value{
@@ -15,9 +14,10 @@ struct value{
 	int tempS_read;	
 };
 
-data getValue();
-
 typedef struct value data;
+data getValue();
+void debug();
+void run();
 
 void loop(){
 	/* run();			// run main program */
@@ -43,18 +43,21 @@ void run(){
 
 	int temp_thr = 39;					// max temperature value before dehydration
 	int wet_thr = 500;					// wet sensor value, max water loss value
+	int impact_thr = 500;				// init impact threshold
 
 	auto d = getValue();			// get data in strut form
 
 	bool dehydrated = d.tempS_read > temp_thr && d.rainDrop_read > wet_thr;			// assuming if temp and wetSensor exceed respective threshold value then dehydrated 
-
 	if ( dehydrated ){
-		digitalWrite(Dehydrate_ind ,HIGH);		// if dehydrated light-up indication LED
-	}
-
-	else
-	{
+		digitalWrite(Dehydrate_ind ,HIGH); }		// if dehydrated light-up indication LED
+	else{
 		digitalWrite(Dehydrate_ind ,LOW);	}
+	
+	bool isSeriousImpact = fsr_read > impact_thr;
+	if (isSeriousImpact){
+		digitalWrite(impactIndicator,HIGH);	}
+	else{
+		digitalWrite(impactIndicator,LOW);	}
 }
 
 data getValue(){
